317565a4a8e360d7075510c4a6a44ac17a5758b4
Vladimir Angelov <vladimirsfile@gmail.com>
2026-01-15 12:37:34 +0200

    Add biggerNum() function to journal.js

--- journal.js
+++ journal.js
@@ -6,13 +6,13 @@ function greet(name) {
 // Print the bigger number
 function biggerNum(num1, num2) {
     if (num1 > num2) {
-        console.log(`${num1} is the bigger number!`);
+        return (`${num1} is the bigger number!`);
     } else if (num1 < num2) {
-        console.log(`${num2} is the bigger number!`);
+        return (`${num2} is the bigger number!`);
     } else {
-        console.log(`${num1} and ${num2} are equal!`);
+        return (`${num1} and ${num2} are equal!`);
     };
 }
 console.log(biggerNum(22, -7));
 console.log(biggerNum(27, 27));
-console.log(biggerNum(16, 164));
+console.log(biggerNum(66, 147));
